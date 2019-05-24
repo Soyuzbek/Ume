12d9fbde9a034d6e39c65590d1ca0b92c3111cc5
soyuzbek <soyuzbek196.kg@gmail.com>
2019-05-24 13:51:22 +0600

    ignore changed

--- users/admin.py
+++ users/admin.py
@@ -3,11 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 from modeltranslation.admin import TabbedTranslationAdmin
 from django.utils.translation import gettext_lazy as _
 
-from users.models import User
+from users.models import User, Student, Teacher
 
 
 @admin.register(User)
-class StudentAdmin(TabbedTranslationAdmin):
+class UserAdmin(TabbedTranslationAdmin):
     fieldsets = ((None, {'fields': ('id', 'username', 'password', )}),
                  (_('Personal info'), {'fields': ('is_student', 'is_teacher')}))
 
+@admin.register(Student)
+class StudentAdmin(TabbedTranslationAdmin):
+    exclude = None
+
+@admin.register(Teacher)
+class TeacherAdmin(TabbedTranslationAdmin):
+    exclude = None
--- users/translation.py
+++ users/translation.py
@@ -1,10 +1,18 @@
 from modeltranslation.decorators import register
 from modeltranslation.translator import TranslationOptions
 
-from users.models import User
+from users.models import User, Student, Teacher
 
 
 @register(User)
 class UserTransOptions(TranslationOptions):
     # fields = (, )
+    pass
+
+@register(Student)
+class StudentOptions(TranslationOptions):
+    pass
+
+@register(Teacher)
+class TeacherOption(TranslationOptions):
     pass
